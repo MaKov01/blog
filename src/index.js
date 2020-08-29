fbfb46254dbb13373e79cdda6d6c19cc3757f7ac
Valentin Caligiuri <makox.coder@gmail.com>
2020-08-29 09:24:48 -0300

    make it better

--- src/index.js
+++ src/index.js
@@ -9,6 +9,3 @@ app.use(express.json());
 app.use(require("./routes/index"));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(10843, () => {
-  console.log("Server on port 10843");
-});
