aa24c822a4a76672cae001aa29b62d5f99f7f82b
Valentin Caligiuri <makox.coder@gmail.com>
2020-08-29 09:35:59 -0300

    make it better

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const nodemailer = require("nodemailer");
 const router = Router();
 
-router.post("/email-send", async (req, res) => {
+router.post("/enviado.html", async (req, res) => {
   const { name, message, email } = req.body;
 
   contentHTML = `
