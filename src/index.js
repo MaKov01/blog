f061f3d0b669739adfe54b8fb4ce42e1aa55c393
Valentin Caligiuri <makox.coder@gmail.com>
2020-08-29 10:27:18 -0300

    make it better

--- src/index.js
+++ src/index.js
@@ -7,6 +7,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(require("./routes/index"));
 app.use(express.static(path.join(__dirname, "public")));
-var port_number = app.listen(3000);
+var port_number = app.listen(process.env.PORT || 3000);
 app.listen(port_number);
-console.log("Server on port", port_number)
